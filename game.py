d4ff388d3cfba51c3b47cdd74be4cbdc2bdf0fbb
a-dubs <alecwarren19@gmail.com>
2023-04-01 13:54:18 -0400

    added Person class and bullets do damage

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
+from typing import Optional, Any
 import arcade
 import arcade.color as colors
-from arcade import Sprite,SpriteList,SpriteCircle
+from arcade import Sprite,SpriteList,SpriteCircle, Texture
 from random import random, randint, randrange
 from math import cos, tan, sin, pi
+
 
 # https://realpython.com/arcade-python-game-framework/
 
@@ -19,11 +21,38 @@ PLAYER_SCALE = 0.2
 # ENEMY_WIDTH = 50
 # PLAYER_HEIGHT = 50
 # PLAYER_WIDTH = 50
+PLAYER_HEALTH_NORMAL = 1000
+ENEMY_HEALTH_NORMAL = 200
+BULLET_DMG = 100
 
 BULLET_SPEED = 35.0
 FRIENDLY_BULLET_COLOR = colors.GOLDEN_POPPY
+FRIENDLY_BULLET_RADIUS = 5
+ENEMY_BULLET_RADIUS = 4
 ENEMY_BULLET_COLOR = colors.RED_PURPLE
 
+
+class Person(arcade.Sprite):
+
+    def __init__(self, hp : int, filename: str = None, scale: float = 1, image_x: float = 0, image_y: float = 0, image_width: float = 0, image_height: float = 0, center_x: float = 0, center_y: float = 0, repeat_count_x: int = 1, repeat_count_y: int = 1, flipped_horizontally: bool = False, flipped_vertically: bool = False, flipped_diagonally: bool = False, hit_box_algorithm: Optional[str] = "Simple", hit_box_detail: float = 4.5, texture: Texture = None, angle: float = 0):
+        super().__init__(filename, scale, image_x, image_y, image_width, image_height, center_x, center_y, repeat_count_x, repeat_count_y, flipped_horizontally, flipped_vertically, flipped_diagonally, hit_box_algorithm, hit_box_detail, texture, angle)
+        self.hp = hp
+
+    def update(self):
+        if self.hp <= 0:
+            self.kill()
+        super().update()
+
+    def take_damage(self,damage:int):
+        self.hp -= damage
+
+# class PersonList(SpriteList):
+#     def __init__(self, use_spatial_hash=None, spatial_hash_cell_size=128, is_static=False, atlas: "TextureAtlas" = None, capacity: int = 100, lazy: bool = False, visible: bool = True):
+#         super().__init__(use_spatial_hash, spatial_hash_cell_size, is_static, atlas, capacity, lazy, visible)
+    
+#     def append(self, person: Person):
+#         return super().append(sprite)
+    
 # Classes
 class ColdpointSeattle(arcade.Window):
     """Main welcome window
@@ -40,7 +69,7 @@ class ColdpointSeattle(arcade.Window):
         # set up the empty sprite lists
         self.enemies_list = arcade.SpriteList()
         self.players_list = arcade.SpriteList()
-        self.obstacles_list = arcade.SpriteList()
+        self.obstacles_list = arcade.SpriteList(use_spatial_hash=True)
         self.projectiles_list = arcade.SpriteList()
         # self.all_sprites = arcade.SpriteList()
         
@@ -51,19 +80,19 @@ class ColdpointSeattle(arcade.Window):
         # setup up background
         arcade.set_background_color(arcade.color.WHITE_SMOKE)
         
-        
         # set up player
-        self.player = arcade.Sprite("resources/sprites/player.png",scale=PLAYER_SCALE)
+        self.player = Person(hp=PLAYER_HEALTH_NORMAL,filename="resources/sprites/player.png",scale=PLAYER_SCALE)
         self.player.center_x =  CENTER_X
         self.player.center_y =  CENTER_Y
         self.players_list.append(self.player)
 
         # Spawn a new enemy every  seconds
-        arcade.schedule(self.spawn_enemy, 1)
+        # arcade.schedule(self.spawn_enemy, 1)
+        arcade.schedule(self.make_enemies_shoot, 1)
 
-    def spawn_enemy(self, d_time: float):
+    def spawn_enemy(self, d_time: float=None):
         # enemy = arcade.Sprite("resources/sprites/enemy.png",image_height=ENEMY_HEIGHT,image_width=ENEMY_WIDTH)
-        enemy = arcade.Sprite("resources/sprites/enemy2.png",scale=ENEMY_SCALE)
+        enemy = Person(hp=ENEMY_HEALTH_NORMAL,filename="resources/sprites/enemy2.png",scale=ENEMY_SCALE)
         # enemy.center_x = CENTER_X + 0.75 * CENTER_X * (dir:=(-1)**randint(0,1)) + 0.01*randint(0,25) * CENTER_X * dir
         # enemy.center_y = CENTER_Y + 0.75 * CENTER_Y * (dir:=(-1)**randint(0,1)) + 0.01*randint(0,25) * CENTER_Y * dir
         if randint(0,1):
@@ -82,6 +111,10 @@ class ColdpointSeattle(arcade.Window):
         self.enemies_list.append(enemy)
         # self.all_sprites.append(enemy)
 
+    def make_enemies_shoot(self, d_time: float):
+        for e in self.enemies_list:
+            self.shoot_from(e)
+
     def on_draw(self):
         """Called whenever you need to draw your window
         """
@@ -95,8 +128,9 @@ class ColdpointSeattle(arcade.Window):
         self.obstacles_list.draw()
         self.projectiles_list.draw()
 
-    def shoot(self, sprite : arcade.Sprite):
-        bullet = SpriteCircle(4,color=(FRIENDLY_BULLET_COLOR if sprite.position in [p.position for p in self.players_list] else ENEMY_BULLET_COLOR))
+    def shoot_from(self, sprite : arcade.Sprite):
+        bullet = SpriteCircle(FRIENDLY_BULLET_RADIUS if sprite in self.players_list else ENEMY_BULLET_RADIUS,
+                              color=(FRIENDLY_BULLET_COLOR if sprite in self.players_list else ENEMY_BULLET_COLOR))
         # bullet.center_x = sprite.center_x + sprite.width * 0.65 * cos(sprite.radians+pi*0.44)
         # bullet.center_y = sprite.center_y + sprite.height * 0.65 * sin(sprite.radians+pi*0.44)
         bullet.center_x = sprite.center_x + sprite.width * 0.5 * cos(sprite.radians+0.5*pi)
@@ -108,16 +142,22 @@ class ColdpointSeattle(arcade.Window):
         # bullet.forward(10)
         self.projectiles_list.append(bullet)
         # bullet.color
+
+    def end_game(self):
+        pass
         
 
     def updateProjectiles(self):
         for proj in self.projectiles_list:
-            players = None
-            enemies = None
-            if ((proj.color == FRIENDLY_BULLET_COLOR and (enemies:=proj.collides_with_list(self.enemies_list))) 
-                or (proj.color == ENEMY_BULLET_COLOR and (players:=proj.collides_with_list(self.enemies_list)))):
+            # if ((proj.color == FRIENDLY_BULLET_COLOR and (enemies:=proj.collides_with_list(self.enemies_list))) 
+                # or (proj.color == ENEMY_BULLET_COLOR and (players:=proj.collides_with_list(self.players_list)))):
+            if proj.width/2 == FRIENDLY_BULLET_RADIUS and (enemies:=proj.collides_with_list(self.enemies_list)):
                 if enemies:
-                    [e.kill() for e in enemies]  
+                    [e.take_damage(BULLET_DMG) for e in enemies]  
+                proj.kill()
+            if proj.width/2 == ENEMY_BULLET_RADIUS and (players:=proj.collides_with_list(self.players_list)):
+                if players:
+                    [p.take_damage(BULLET_DMG) for p in players]  
                 proj.kill()
             elif (obstacle:=proj.collides_with_list(self.obstacles_list)):
                 proj.kill()
@@ -131,6 +171,7 @@ class ColdpointSeattle(arcade.Window):
 
         if symbol == arcade.key.ESCAPE:
             self.paused = not self.paused
+            print(self.paused)
 
         if symbol == arcade.key.W or symbol == arcade.key.UP:
             self.player.change_y = 5
@@ -145,7 +186,7 @@ class ColdpointSeattle(arcade.Window):
             self.player.change_x = 5
 
         if symbol == arcade.key.SPACE:
-            self.shoot(self.players_list[0])
+            self.shoot_from(self.players_list[0])
 
     
     def on_update(self, delta_time: float):
@@ -163,11 +204,14 @@ class ColdpointSeattle(arcade.Window):
         self.player.change_x = 1.0 * cos(self.player.radians+0.5*pi)
         self.player.change_y = 1.0 * sin(self.player.radians+0.5*pi)
 
+        
+
         # Update everything
         self.players_list.update()
         self.enemies_list.update()
         self.projectiles_list.update()
         self.obstacles_list.update()
+        self.updateProjectiles()
 
         # Keep the player on screen
         if self.player.top > self.height:
